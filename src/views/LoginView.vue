e9e30039712badd154a57a5ba0bf711c202240a2
ENPetterson <e.n.petterson@gmail.com>
2023-07-18 17:02:57 -0300

    Vue Base

---
+++ src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <div>
+        {{ usuario }}
+        <h1>Login</h1>
+        <label for="e">Ingresar Correo:</label>
+        <input type="email" v-model="usuario.email">
+        <br>
+        <label>Ingresar Contraseña:</label>
+        <input type="password" v-model="usuario.password">
+        <br>
+        <input type="button" value="Ingresar" v-on:click="funIngresar()">
+    </div>
+
+</template>
+
+<script setup>
+    import { ref } from "vue";
+    import axios from "axios";
+
+    /*
+    const correo = ref("aa@gmail.com");
+    const clave = ref("aa");
+    */
+    const usuario = ref({email: "", password: ""});
+
+    function funIngresar() {
+        axios.post("http://127.0.0.1:8000/api/v1/auth/login", usuario.value).then(
+            (res) => {
+                console.log(res)
+            }
+        )
+    }
+
+</script>
+
+
+<style>
+
+</style>
